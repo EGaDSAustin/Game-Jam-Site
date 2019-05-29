4c4f8b9a6ca5fdf10e1661baf88bc25c0dcdf7e1
imcdo <ian_mcdonald@rocketmail.com>
2019-05-29 16:23:38 -0500

    update install scripts

--- scripts/dev-install.sh
+++ scripts/dev-install.sh
@@ -1,8 +1,9 @@
 # Get all the stuff you need by running this script
 # Run with sudo
+echo "!UPDATE apt";
 apt update;
-apt-get update;
 
+echo "!INSTALLING node";
 apt install nodejs;
 nodejs --version;
 apt install npm;
@@ -10,10 +11,12 @@ apt install npm;
 # dont remember if i did that or this:
 # curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
+echo "!INSTALLING ruby and travis";
 apt install -y ruby ruby-dev;
 gem install travis;
 
 # Get heroku
+echo "!INSTALLING heroku";
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh;
 
 npm install --save-dev;
