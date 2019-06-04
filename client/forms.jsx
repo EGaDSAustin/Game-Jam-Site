c5d33a5ff9adde6c1024dae6e37850cf839a14e3
imcdo <ian_mcdonald@rocketmail.com>
2019-06-04 16:31:25 -0500

    fix react form stuff

--- client/forms.jsx
+++ client/forms.jsx
@@ -1,18 +1,54 @@
 import React from 'react'
 
 class TestForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: null,
+            email: null,
+            pass: null
+        };
+
+        this.changeNameHandler = this.changeNameHandler.bind(this);
+        this.changeEmailHandler = this.changeEmailHandler.bind(this);
+        this.changePassHandler = this.changePassHandler.bind(this);
+    }
+
+    changeNameHandler(event) {
+        this.setState({
+            name: event.target.value,
+        });
+    }
+
+    changeEmailHandler(event) {
+        this.setState({
+            email: event.target.value,
+        });
+    }
+
+    changePassHandler(event) {
+        this.setState({
+            pass: event.target.value,
+        });
+    }
+
 
     render() {
+
+
         return (
             <div className="testform">
-                <form>
+                <form onSubmit={e => {
+                    alert('SUMBIT');
+                    event.preventDefault();
+                }}>
                     Name:
-                    <input type="text"></input> <br />
+                    <input type="text" value={this.state.name} onChange={this.changeHandler} /> <br />
                     Email:
-                    <input type="email"></input> <br />
+                    <input type="email" value={this.state.email} onChange={this.changeHandler} /> <br />
                     Pass:
-                    <input type="password"></input> <br />
-                    <input type="submit" value="Submit"></input>
+                    <input type="password" value={this.state.pass} onChange={this.changeHandler} /> <br />
+                    <input type="submit" value="Submit" />
                 </form>
             </div>
         );
