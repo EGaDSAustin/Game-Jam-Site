225ca67cd08c4e9a5ef75fd2fc39e85251afbcee
crmontminy <colette.montminy@gmail.com>
2019-06-30 16:32:09 +0300

    added game jam games section

---
+++ client/components/Games.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import { Typography, Grid, Card, CardMedia, Container } from '@material-ui/core'
+
+export class Games extends React.Component {
+    render(){
+        return(
+            <Container maxWidth="lg">
+                <Typography variant="h3" align="center">
+                    Previous Game Jam Games
+                </Typography>
+                <br/>
+            	<Grid container spacing={4}>
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card> 
+                            <CardMedia
+                                component="img"
+                                image="https://img.itch.zone/aW1hZ2UvMzA2MDIxLzE1MDE3OTUucG5n/347x500/LrTq%2BV.png"
+                            />
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"        
+                            />
+                        </Card>
+                    </Grid>
+                    
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"
+                            />
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"   
+                            />
+                        </Card>
+                    </Grid>
+                    
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"
+                            />
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"
+                                
+                            />
+                        </Card>
+                    </Grid>
+                    
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"
+                            />
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"
+                                
+                            />
+                        </Card>
+                    </Grid>
+                    
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"
+                            />
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"
+                                
+                            />
+                        </Card>
+                    </Grid>
+                    
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"
+                            />
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={6} sm={4} md={3} lg={2}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image="https://www.clickertraining.com/files/u1/lab_puppy_250.jpg"
+                                
+                            />
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Container>
+        );
+    }
+}
