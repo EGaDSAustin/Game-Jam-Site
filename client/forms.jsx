bf14387220a758e145b7e442ac5da1c091a4390c
crmontminy <colette.montminy@gmail.com>
2019-07-25 23:54:15 -0400

    cleaned up forms and made them work better

--- client/forms.jsx
+++ client/forms.jsx
@@ -1,50 +1,119 @@
 import React, { useState } from 'react'
+// import TextField from '@material-ui/core/TextField';
 
-
-function Field({type, index, field, updateField}) {
+function Field({ type, index, field, updateField }) {
     const [value, setValue] = useState('');
 
     return (
         <div>
             <h3><strong>hi field {index+1} this field is: </strong> <a style={{color: 'black'}}>{field.value}</a> </h3> 
-            
-            <form onSubmit={e => {
-                e.preventDefault();
+            <label>{field.name}: </label>
+            <input type={type} value={value} 
+            onBlur={ e => {
+                setValue(e.target.value);
                 updateField(index, value);
-            }}>
-                <label>{field.name}: </label>
-                <input type={type} value={value} onChange={e => {setValue(e.target.value)}}/>
-            </form>
+            }}
+            onChange={e => {
+                setValue(e.target.value);
+            }}/>
         </div>
     );
 }
 
 export function TestForm(props){
     const [fields, setFields] = useState([
+        // Required
         {
             name: 'Email',
             value: null,
-            type: "text"
+            type: "email",
+            required: true
         },
         {
             name: 'First Name',
             value: null,
-            type: "text"
+            type: "text",
+            required: true
         },
         {
             name: 'Last Name',
             value: null,
-            type: "text"
+            type: "text",
+            required: true
         },
         {
-            name: 'School',
+            name: 'Pronoun',
             value: null,
-            type: 'text'
+            type: "text",
+            required: true
         },
         {
             name: 'Password',
             value: null,
-            type: 'password'
+            type: "password",
+            required: true
+        },
+        {
+            name: 'School',
+            value: null,
+            type: "text",
+            required: true
+        },
+        {
+            name: 'Major',
+            value: null,
+            type: "text",
+            required: true
+        },
+        {
+            name: 'Year', // Are you trying to do year Started? Graduated? Started College? Fresh/Soph/Jun/Sen/Grad?
+            value: null,  
+            type: "month", // could also be text or number depending on ^
+            required: true 
+        },
+        {
+            name: 'Experience Level',
+            value: null,
+            type: "text",
+            required: true
+        },
+        {
+            name: 'Preferred Disciplines',
+            value: null,
+            type: "text",
+            required: true
+        },
+        {
+            name: 'Resume',
+            value: null,
+            type: "url",
+            required: true
+        },
+        // Optional
+        {
+            name: 'Preferred Teammates',
+            value: null,
+            type: "text",
+            required: false
+        },
+        {
+            name: 'Portfolio Link',
+            value: null,
+            type: "url",
+            required: false
+            
+        },
+        {
+            name: 'Github Link',
+            value: null,
+            type: "url",
+            required: false
+        },
+        {
+            name: 'LinkedIn Link',
+            value: null,
+            type: "url",
+            required: false,
         },
     ]);
 
@@ -53,15 +122,35 @@ export function TestForm(props){
         const newField = newFields[index];  
         newField.value = value;
         newFields[index] = newField;
-        setFields(newFields);
+        setFields(newFields); 
     };
+    // errorstr = `missing required field ${field.name}`;
     
+    // TODO: STEVE AND COLETTE DO THIS
+    const checkRequiredAndValid = () => {
+        foreach(field in fields) {
+            if (field.required && field.value == null) {
+                // WOOPSIES
+            } else {
+                // WE GOOD HOMMIE
+            }
+            // run correct validation method
+        }
+    };
+
     return (
     <div>
         <hi>FORM with {fields.length}</hi>
-           {fields.map((field, index) => (
+        <form onSubmit={/*TODO: on submit here*/ e => {
+            e.preventDefault();
+            // TODO: got stuff to do
+            checkRequiredAndValid(); 
+        }}>
+            {fields.map((field, index) => (
                 <Field type={field.type} index={index} field={field} updateField={updateField} />
             ))}
+             <input type='submit'/>
+        </form>
     </div>
     );
 }
