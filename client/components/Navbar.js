7a129568c73927d2319c7b4c2e2b4d79597e83d5
Steve Wang <stevewang@utexas.edu>
2019-07-28 23:05:27 -0700

    Navbar correctly follows scrolling page

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -11,16 +11,19 @@ export class Navbar extends React.Component {
 
     render() {
         return (
-            <AppBar position='sticky'>
-                <Tabs>
-                    <Tab value={0} label="Home" to="/" component={Link}/>
-                    <Tab value={1} label="About"/>
-                    <Tab value={2} label="Register" to="/form" component={Link}/>
-                    <Tab value={3} label="Schedule"/>
-                    <Tab value={4} label="Games"/>
-                    <Tab value={5} label="Sponsors"/>
-                </Tabs>
-            </AppBar>
+            <React.Fragment>
+                <AppBar>
+                    <Tabs>
+                        <Tab value={0} label="Home" to="/" component={Link}/>
+                        <Tab value={1} label="About"/>
+                        <Tab value={2} label="Register" to="/form" component={Link}/>
+                        <Tab value={3} label="Schedule"/>
+                        <Tab value={4} label="Games"/>
+                        <Tab value={5} label="Sponsors"/>
+                    </Tabs>
+                </AppBar>
+                <Toolbar />
+            </React.Fragment>
         );
     }
 }
