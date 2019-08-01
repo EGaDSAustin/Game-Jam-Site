402972ae8d23010fc4eac1716efc96825edb2578
imcdo <ian_mcdonald@rocketmail.com>
2019-08-01 01:42:24 -0500

    validation begining

--- client/forms.jsx
+++ client/forms.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FormControl, Input, InputLabel } from '@material-ui/core';
-
+import { withRouter } from 'react-router-dom';
 
 function Field({ type, index, field, updateField }) {
     const [value, setValue] = useState('');
@@ -40,93 +40,108 @@ export function TestForm(props){
             value: null,
             type: "email",
             required: true,
+            validate: () => true,
             // validate: () => true,
         },
         {
             name: 'First Name',
             value: null,
             type: "text",
-            required: true
+            required: true,
+            validate: () => true
         },
         {
             name: 'Last Name',
             value: null,
             type: "text",
-            required: true
+            required: true,
+            validate: () => true
         },
         {
             name: 'Pronoun',
             value: null,
             type: "text",
-            required: true
+            required: true,
+            validate: () => true
         },
         {
             name: 'Password',
             value: null,
             type: "password",
-            required: true
+            required: true,
+            validate: () => true
         },
         {
             name: 'School',
             value: null,
             type: "text",
-            required: true
+            required: true,
+            validate: () => true
         },
         {
             name: 'Major',
             value: null,
             type: "text",
-            required: true
+            required: true,
+            validate: () => true
         },
         {
             name: 'Year', // Are you trying to do year Started? Graduated? Started College? Fresh/Soph/Jun/Sen/Grad?
             value: null,  
             type: "month", // could also be text or number depending on ^
-            required: true 
+            required: true ,
+            validate: () => true
         },
         {
             name: 'Experience Level',
             value: null,
             type: "text",
-            required: true
+            required: true,
+            validate: () => true
         },
         {
             name: 'Preferred Disciplines',
             value: null,
             type: "text",
-            required: true
+            required: true,
+            validate: () => true
         },
         {
             name: 'Resume',
             value: null,
             type: "url",
-            required: true
+            required: true,
+            validate: () => true
         },
         // Optional
         {
             name: 'Preferred Teammates',
             value: null,
             type: "text",
-            required: false
+            required: false,
+            validate: () => true
         },
         {
             name: 'Portfolio Link',
             value: null,
             type: "url",
-            required: false
+            required: false,
+            validate: () => true
             
         },
         {
             name: 'Github Link',
             value: null,
             type: "url",
-            required: false
+            required: false,
+            validate: () => true
         },
         {
             name: 'LinkedIn Link',
             value: null,
             type: "url",
-            required: false
+            required: false,
+            validate: () => true
         },
     ]);
 
@@ -140,15 +155,17 @@ export function TestForm(props){
     // errorstr = `missing required field ${field.name}`;
     
     // TODO: STEVE AND COLETTE DO THIS
+    // TODO: error fields when they dont work owo
     const checkRequiredAndValid = () => {
         const errorFields = [];
-        for(let field of fields) {
-            if (field.required && field.value == null) {
+        for(const field of fields) {
+            if (field.required && field.value == null && field.validate()) {
                 errorFields.push(field.name);
-                
                 //alert("Please don't be mad ;-;")
+                return false;
             } else {
                 // WE GOOD HOMMIE
+                return true;
             }
             console.log({errorFields});
             // run correct validation method
@@ -161,7 +178,11 @@ export function TestForm(props){
         <form onSubmit={e => {
             e.preventDefault();
             // TODO: got stuff to do
-            checkRequiredAndValid(); 
+            if(checkRequiredAndValid()) { //LULW hypersomegapoggerscrazy Kappa
+                // go to next page
+                // this.props.history.push('/programming-so-easy');
+                // route here like above but actually working :)
+            }
         }}>
             {fields.map((field, index) => (
                 <Field type={field.type} index={index} field={field} updateField={updateField} />
@@ -171,4 +192,3 @@ export function TestForm(props){
     </div>
     );
 }
-
