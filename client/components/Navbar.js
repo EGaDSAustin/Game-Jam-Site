326d05eed7de4fa3b7cd73191c0060d926b034da
Ali Kedwaii <ali.kedwaii@utexas.edu>
2019-08-07 20:29:11 -0500

    scrolling still not working uggh

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
 
 import AppBar from '@material-ui/core/AppBar';
 import { Tabs, Tab, Toolbar } from '@material-ui/core';
@@ -15,11 +16,11 @@ export class Navbar extends React.Component {
                 <AppBar>
                     <Tabs>
                         <Tab value={0} label="Home" to="/" component={Link}/>
-                        <Tab value={1} label="About"/>
-                        <Tab value={2} label="Register" to="/form" component={Link}/>
-                        <Tab value={3} label="Schedule"/>
-                        <Tab value={4} label="Games"/>
-                        <Tab value={5} label="Sponsors"/>
+                        <Tab value={1} label="About" to="/#about" component={Link}/>
+                        <Tab value={2} label="Register" to="register" component={Link}/>
+                        <Tab value={3} label="Schedule" to="/#schedule" component={Link}/>
+                        <Tab value={4} label="Games" to="/#games" component={Link}/>
+                        <Tab value={5} label="Sponsors" to="/#sponsors" component={Link}/>
                     </Tabs>
                 </AppBar>
                 <Toolbar />
