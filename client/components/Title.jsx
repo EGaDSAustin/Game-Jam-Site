4c7f0caf513b63e22f6e41d859a199d8d51caa43
Courtney Huynh <qwertyhwin@gmail.com>
2019-08-24 15:16:26 -0500

    used grid for spacing the info grid and renamed some game files

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -33,42 +33,46 @@ export class Title extends React.Component {
                 <Typography variant="subtitle1">36 hours</Typography>
 
                 <img src = {fallingPeople} height = "100%" width = "100%"/>
-                <Typography variant="h3" style={{marginTop: '50px'}}>Come join us in making games,</Typography>
+                <Typography variant="h3" style={{paddingTop: '50px'}} id="about">Come join us in making games,</Typography>
                 <Typography variant="h3">friends, and memories!</Typography>
-                <div className="infoRow" id="about">
-                    <InfoCard
-                        category='Who?'
-                        photo={whoImage} alt_text='who_image'
-                        description= {
-                            <div>
-                                <Typography variant="subtitle1">This event is hosted by <a href="https://test-egads-website.herokuapp.com/">EGaDS!</a></Typography>
-                                <Typography variant="subtitle1">The Electronic Game Developers Society</Typography>
-                                <Typography variant="subtitle1">is a student organization at UT Austin.</Typography>
-                            </div>
-                        }
-                        />
-                    <InfoCard
-                        category='What?'
-                        photo={whatImage} alt_text='what_image'
-                        description= "UT Game Jam is a 36 hour event where\nparticipants work in teams to create a\nvideo game given a theme."/>
-                    {/* <div className="infoCard">
-                        <img src={whoImage} alt='who_image' style={styles.image}/>
-                        <Typography variant="h2">Who?</Typography>
-                        <Typography variant="subtitle1">This event is hosted by <a href="https://test-egads-website.herokuapp.com/">EGaDS!</a></Typography>
-                        <Typography variant="subtitle1">The Electronic Game Developers Society is a student organization at UT Austin.</Typography>
-                    </div> */}
-                </div>
+                <Grid container spacing={9}>
+                    <Grid item xs={6}>
+                        <InfoCard
+                            category='Who?'
+                            photo={whoImage} alt_text='who_image'
+                            description= {
+                                <div>
+                                    <Typography variant="subtitle1">This event is hosted by <a href="https://test-egads-website.herokuapp.com/">EGaDS!</a></Typography>
+                                    <Typography variant="subtitle1">The Electronic Game Developers Society</Typography>
+                                    <Typography variant="subtitle1">is a student organization at UT Austin.</Typography>
+                                </div>
+                            }/></Grid>
+                        <Grid item xs={6}>
+                        <InfoCard
+                            category='What?'
+                            photo={whatImage} alt_text='what_image'
+                            description= "UT Game Jam is a 36 hour event where\nparticipants work in teams to create a\nvideo game given a theme."/>
+                        {/* <div className="infoCard">
+                            <img src={whoImage} alt='who_image' style={styles.image}/>
+                            <Typography variant="h2">Who?</Typography>
+                            <Typography variant="subtitle1">This event is hosted by <a href="https://test-egads-website.herokuapp.com/">EGaDS!</a></Typography>
+                            <Typography variant="subtitle1">The Electronic Game Developers Society is a student organization at UT Austin.</Typography>
+                        </div> */}
+                        </Grid>
+                    </Grid>
                 
-                <div className="infoRow" id="register">
+                <Grid container spacing={4} justify="center">
+                    <Grid item xs={5}>
                     <InfoCard
                         category='Where?'
                         photo={whereImage} alt_text='where_image'
-                        description= "Multiple locations around\nThe University of Texas at\nAustin. See schedule for\nmore details!"/>
+                        description= "Multiple locations around\nThe University of Texas at\nAustin. See schedule for\nmore details!"/></Grid>
+                    <Grid item xs={5}>
                     <InfoCard
                         category='Why?'
                         photo={whyImage} alt_text='why_image'
-                        description= "Learn new skills and/or\nhone existing ones\nwhile making your own\ngame!"/>
-                </div>
+                        description= "Learn new skills and/or\nhone existing ones\nwhile making your own\ngame!"/></Grid>
+                </Grid>
                   <img src = {RegisterImage} alt='register image' style={{maxWidth: '100%', width: 'auto', position: 'relative', top: '-150px', marginBottom: '-150px'}}/>
             </Container>
         );
