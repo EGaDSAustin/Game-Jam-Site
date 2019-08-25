a5bb22b75fd6b885ef4484319aa5a200e3a26dda
Courtney Huynh <qwertyhwin@gmail.com>
2019-08-24 23:33:26 -0500

    center the register button

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -1,35 +1,28 @@
 import React from 'react';
-import { Container, Button } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom'
-import styled from 'styled-components';
 import Fab from '@material-ui/core/Fab';
-import sparkle1 from '../assets/sparkle1.JPG';
+import sparkle1 from '../assets/sparkle1.PNG';
 import sparkle2 from '../assets/sparkle2.JPG';
-
-const StyledContainer = styled(Container)`
-    padding-top: 0;
-`;
 
 const styles = {
   image: {
-    maxWidth: '100%',
-    width: 'auto',
+    width: '76px', // enforce the same width so that register button is centered
+    height: 'auto',
     verticalAlign: 'middle',
-    padding: '20px'
+    padding: '0px 20px'
   }
 };
 
 export class Register extends React.Component {
     render(){
         return(
-          
-            <StyledContainer>
+            <div style={{paddingTop: '0'}}>
               <img src = {sparkle1} alt='left sparkles' style={styles.image}/>
               <Fab variant="extended" aria-label="add" color="primary" to="/register" component={RouterLink}>
                 Register
               </Fab>
               <img src = {sparkle2} alt='right sparkles' style={styles.image}/>
-            </StyledContainer>
+            </div>
         );
     }
 }
